f393ce2d2c0b1ebb0c144c5144bfe2c39d24949e
Andrew Ma <maastergotmail@gmail.com>
2015-10-06 01:18:48 -0400

    lots of things added

--- website/client/scripts/test.js
+++ website/client/scripts/test.js
@@ -3,17 +3,31 @@
  */
 Template.test.events({
    'change input': function(e) {
-       files = $("input[type='file']")[0].val();
-       console.log(files);
-       e.preventDefault();
-       e.stopPropagation();
-       var reader = new FileReader();
-       console.log(files);
-       //reader.onloadend = function() {
+       files = $("input[type='file']")[0].files[0];
+       // console.log(files);
+       // e.preventDefault();
+       // e.stopPropagation();
+       // var reader = new FileReader();
+       // console.log(files);
+       // //reader.onloadend = function() {
            //console.log(reader.result);
-       Meteor.call('uploadEchoFile', files, "IQ8ICKDOZGGE74XJQ");
-
-       //};
-
+       // Meteor.call('uploadEchoFile', files, "IQ8ICKDOZGGE74XJQ", function(url){
+       //    console.log("this is dumb " + url);
+          
+       // });
+      Meteor.call('uploadEchoFile', files, "IQ8ICKDOZGGE74XJQ", iFuckingHateMeteor);
+       
    }
 });
+
+function iFuckingHateMeteor(jsonURL){
+  Meteor.http.call("GET",
+    jsonURL,
+    function(error, result){
+      if(result.statusCode === 200){
+        console.log("fuck this shit");
+        console.log(result);
+      }
+    }
+  );
+}
--- website/server/echonest.js
+++ website/server/echonest.js
@@ -1,14 +1,56 @@
 Meteor.methods({
-    uploadEchoFile: function(file, apiKey) {
 
+    uploadEchoFile: function(file, apiKey, callback) {
+        console.log("start");
+        console.log(callback);
+        var jsonURL;
+        var apiKey = "KZVTGHHVOTXY6XXYA";
         var url = "http://developer.echonest.com/api/v4/track/upload";
-        console.log(apiKey);
-        var options = {
-            header: {'Content-Type': 'multipart/form-data'},
-            content: file
-        };
-        HTTP.call("POST", "http://developer.echonest.com/api/v4/track/upload?api_key=" + apiKey + "&filetype=mp3", options, function(err, res) {
-            console.log(res);
-        });
+        console.log("\n\n\n\n\n\n\n\n\n");
+        track = "http://tylergrund.com/mp3/MP3s%20from%20home/Coldplay/01%20Speed%20Of%20Sound.mp3";
+        Meteor.http.call("POST",
+            "http://developer.echonest.com/api/v4/track/upload?api_key=" + apiKey + "&url=" + track,
+            {
+                data:{
+                    "api_key": apiKey,
+                    "url": track
+                },
+                headers:{
+                    "content-type": "application/octet-stream"
+                }
+            },
+            function(error, result){
+                if (result.statusCode === 200) {
+                    var trackID = result.data.response.track.id;
+                    jsonURL = Meteor.call('meteorisGay', apiKey, trackID, callback);
+                }
+            }
+        );
+    },
+
+    meteorisGay: function(apiKey, trackID, callback){
+        var jsonURL;
+        Meteor.http.call("GET",
+            "http://developer.echonest.com/api/v4/track/profile?api_key=" + apiKey + "&format=json&id=" + trackID + "&bucket=audio_summary",
+            {
+                data:{
+                    "api_key": apiKey,
+                    "url": track
+                },
+                headers:{
+                    "content-type": "application/octet-stream"
+                }
+            },
+            function(error, result){
+                if (result.statusCode === 200) {
+                    jsonURL = result.data.response.track.audio_summary.analysis_url;
+                    console.log(jsonURL);
+                    console.log("please leave");
+                    console.log(callback);
+                    callback(jsonURL);
+                }
+            }
+        );
+        return jsonURL;
     }
 });
