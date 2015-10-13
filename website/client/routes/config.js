64ef98fb4c1a2e36e9bc04fab37ac0069bdc3c49
Chinchalupa <wrightjt@rose-hulman.edu>
2015-10-13 09:04:41 -0400

    Added a not found template but not rendering properly possibly due to an issue within the meteor update. Routes now exist for most of the locations, but will need to be revisited for the playlists. Navbar now utilizes proper yielding and all pages that were using a temporary implementation of the navbar have been updated.

---
+++ website/client/routes/config.js
@@ -0,0 +1,9 @@
+/**
+ * Created by wrightjt on 10/12/2015.
+ */
+Router.configure({
+    layoutTemplate: 'nav-bar'
+});
+
+
+
--- website/client/routes/controller.js
+++ website/client/routes/controller.js
@@ -1,7 +1,21 @@
+Router.plugin('dataNotFound', {notFoundTemplate: 'notfound'});
+
 Router.route('/', function() {
    this.render('home');
 });
 
 Router.route('/test', function() {
     this.render('test');
+});
+
+Router.route('/dashboard', function() {
+    this.render('dashboard')
+});
+
+Router.route('/guides', function() {
+    this.render('guides');
+});
+
+Router.route('/playlist', function() {
+   this.render('playlist')
 });
