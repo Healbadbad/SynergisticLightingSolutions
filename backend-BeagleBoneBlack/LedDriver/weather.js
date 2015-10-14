d58e91e380458dc307d578c7db291ad6638bd007
root <root@beaglebone.(none)>
2015-10-14 08:43:28 +0000

    Beaglebone can change the LEDs, a basic node server was setup for testing

---
+++ backend-BeagleBoneBlack/LedDriver/weather.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+var options = {
+host: '
+
+$.ajax({
+  url : "http://api.wunderground.com/api/Your_Key/geolookup/conditions/q/IA/Cedar_Rapids.json",
+  dataType : "jsonp",
+  success : function(parsed_json) {
+  var location = parsed_json['location']['city'];
+  var temp_f = parsed_json['current_observation']['temp_f'];
+  alert("Current temperature in " + location + " is: " + temp_f);
+  }
+});
