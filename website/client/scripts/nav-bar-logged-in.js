125a920d3f05f7a6fd8bfc791aa9e6de441cbdaa
Andrew Ma <maastergotmail@gmail.com>
2015-10-21 15:11:53 -0400

    added reactive var

--- website/client/scripts/nav-bar-logged-in.js
+++ website/client/scripts/nav-bar-logged-in.js
@@ -2,8 +2,33 @@
  * Created by wrightjt on 10/15/2015.
  */
 Template.navBarLoggedIn.events({
-  'click #logout': function(e) {
-      Meteor.logout();
-      Router.go('/');
-  }
+  	'click #logout': function(e) {
+    	Meteor.logout();
+    	Router.go('/');
+  	},
+
+	'click #backButton': function(e, template){
+		template.currentSong.set("testerino");
+  	},
+
+  	'click #nextButton': function(e, template){
+  		template.currentSong.set("plserino");
+  	},
+});
+
+
+// Template.navBarLoggedIn.created = function () {
+// 	this.currentSong = new ReactiveVar("test");
+// };
+
+Template.navBarLoggedIn.onCreated(function(){
+	this.currentSong = new ReactiveVar("test");
+});
+
+Template.navBarLoggedIn.helpers({
+	getCurrentSong: function(){
+		console.log(Template.instance().currentSong.get());
+		return Template.instance().currentSong.get();
+	},
+
 });
