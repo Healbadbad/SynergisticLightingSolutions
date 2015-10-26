064c95104b8babb5dce2d339e31e9426bbfa35d2
Philip Ross <rosspa@rose-hulman.edu>
2015-10-25 23:09:06 -0400

    fixed bug where events didn't trigger on the create playlist modal

--- website/client/scripts/modals/createPlaylistModal.js
+++ website/client/scripts/modals/createPlaylistModal.js
@@ -1,21 +1,12 @@
 Template.createPlaylistModal.events({
     'keyup #new-playlist-name': function(e) {
-        // TODO don't know why this is not being called
-        console.log("called");
-        console.log(e.target.value);
-        //if(exists(e.target.value)) {
-        //    $('#newPasswordCheck').show();
-        //    $('#newPasswordError').hide();
-        //    if(isSamePassword()) {
-        //        deleteErrors();
-        //    } else {
-        //        displayPasswordError();
-        //        $('#rePasswordCheck').hide();
-        //    }
-        //} else {
-        //    $('#newPasswordCheck').hide();
-        //    $('#newPasswordError').show();
-        //}
+        if (e.target.value) {
+            $('#new-playlist-name-error').hide();
+            $('#create-playlist-button').prop('disabled', false);
+        } else{
+            $('#new-playlist-name-error').show();
+            $('#create-playlist-button').prop('disabled', true);
+        }
     },
     'click #create-playlist-button': function(e){
         e.preventDefault();
@@ -26,14 +17,14 @@ Template.createPlaylistModal.events({
         e.stopPropagation();
         console.log("cancer");
     },
-    'click #workPls': function(e) {
+    'click .cancel': function(e) {
         e.preventDefault();
         $('body').trigger({
             type: 'keyup',
             which: 27 // Escape key
         });
         e.stopPropagation();
-    },
+    }
 });
 
 Template.createPlaylistModal.rendered = function(){
