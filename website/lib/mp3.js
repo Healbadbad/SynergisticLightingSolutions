34a42d74111216ec52ecc5bb1e177466f09dca63
chinchalupa <wrightjt@rose-hulman.edu>
2015-10-28 16:03:42 -0400

    Do not touch

--- website/lib/mp3.js
+++ website/lib/mp3.js
@@ -1,3 +1,7 @@
 /**
  * Created by Jeremy on 10/28/2015.
  */
+
+MP3s = new FS.Collection("mp3s", {
+    stores: [new FS.Store.FileSystem("mp3s", {path: Meteor.rootPath + "uploads"})]
+});
--- website/server/routes.js
+++ website/server/routes.js
@@ -1,3 +1,12 @@
 /**
  * Created by Jeremy on 10/28/2015.
  */
+Router.route('/songs/:mp3', function() {
+    var mp3 = this.params.mp3;
+
+    console.log("HOLY");
+    var fs = Npm.require('fs');
+    console.log(process.env.PWD);
+    this.response.end(fs.readFileSync(process.env.PWD + '/uploads/mp3s-SkfovEDoYAKXRcTNh-Baby Baby Goodbye.mp3'));
+
+}, {where: 'server'});
