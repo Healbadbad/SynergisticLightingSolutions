74a151fe42185d8d91d10db889b44030d2ebf403
Philip Ross <rosspa@rose-hulman.edu>
2015-10-29 16:32:29 -0400

    removed extra creation of mongo db

--- website/client/scripts/dashboard.js
+++ website/client/scripts/dashboard.js
@@ -2,11 +2,7 @@
  * Created by rosspa on 10/21/2015.
  */
 
-//Template.dashboard.onCreated(function() {
-//   Meteor.call('addPlaylist', Meteor.userId(), "Test", Meteor.user().username, ['1234'], [Meteor.userId()]);
-//});
 var playlist_index = 0;
-Playlists = new Meteor.Collection('playlists');
 Template.dashboard.helpers({
     getPlaylists: function() {
         return Playlists.find();
