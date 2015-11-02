ef4f00ff065d098cb0bbbba76badd5335558a757
chinchalupa <wrightjt@rose-hulman.edu>
2015-11-02 13:29:41 -0500

    Working uploading and downloading of mp3 files

--- website/lib/mp3.js
+++ website/lib/mp3.js
@@ -1,7 +1,9 @@
 /**
  * Created by Jeremy on 10/28/2015.
  */
-
+fs = Npm.require('fs');
+rootPath = fs.realpathSync('.');
+console.log(rootPath + "..\\..\\..\\.." +  "\\uploads");
 MP3s = new FS.Collection("mp3s", {
-    stores: [new FS.Store.FileSystem("mp3s", {path: Meteor.rootPath + "uploads"})]
+    stores: [new FS.Store.FileSystem("mp3s", {path: rootPath + "\\uploads"})]
 });
