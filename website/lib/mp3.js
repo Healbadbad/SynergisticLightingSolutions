a0747c021b8f32e209dc47839e962a33c1ba2333
chinchalupa <wrightjt@rose-hulman.edu>
2015-11-03 01:15:43 -0500

    Modified ignore file due to local branch error as temp fix.

--- website/lib/mp3.js
+++
@@ -1,8 +0,0 @@
-///**
-// * Created by Jeremy on 10/28/2015.
-// */
-//fs = Npm.require('fs');
-//console.log(rootPath + "..\\..\\..\\.." +  "\\uploads");
-//MP3s = new FS.Collection("mp3s", {
-//    stores: [new FS.Store.FileSystem("mp3s", {path: rootPath + "\\uploads"})]
-//});
