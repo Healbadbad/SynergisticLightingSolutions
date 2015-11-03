18646867ccb967dcf8bafbdefd3d1193e8727708
Andrew Ma <maastergotmail@gmail.com>
2015-11-03 17:19:52 -0500

    ui updates. Now transferirng computers

--- website/client/scripts/nav-bar-logged-in.js
+++ website/client/scripts/nav-bar-logged-in.js
@@ -14,21 +14,45 @@ Template.navBarLoggedIn.events({
   	'click #nextButton': function(e, template){
   		template.currentSong.set("Sandstorm - Darude");
   	},
+
+    'click #playButton': function(e, template){
+      // change play to pause or vice versa, and play/pause song
+      template.playPause.set("fa-pause");
+    },
 });
 
+Template.navBarLoggedIn.rendered = function(){
+    $(document).foundation();
 
-// Template.navBarLoggedIn.created = function () {
-// 	this.currentSong = new ReactiveVar("test");
-// };
+    var playPause = $("#playPauseIcon");
+    $("#playButton").click(function(){
+      console.log("click");
+      isPlaying();
+    });
+    function isPlaying(){
+      var playButton = document.getElementById("playButton");
+      if(playPause.attr("class").indexOf("play") > -1){
+        console.log("pause");
+        playPause.attr("class", "fa fa-pause");
+        playButton.style.fontSize = "70px";
+        playButton.style.top = "-30%";
+      } else {
+        console.log("play");
+        playPause.attr("class", "fa fa-play-circle");
+        playButton.style.fontSize = "100px";
+        playButton.style.top = "-70%";
+      }
+    }
+}
 
 Template.navBarLoggedIn.onCreated(function(){
 	this.currentSong = new ReactiveVar("Sandstorm - Darude");
+  this.playPause = new ReactiveVar("fa-play-circle");
 });
 
 Template.navBarLoggedIn.helpers({
 	getCurrentSong: function(){
 		//console.log(Template.instance().currentSong.get());
 		return Template.instance().currentSong.get();
-	},
-
+	}
 });
