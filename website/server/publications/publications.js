e0f309f5f2038ef250db4218d95763b783cf2d05
chinchalupa <wrightjt@rose-hulman.edu>
2015-11-04 03:37:27 -0500

    Added the full playlist page. Need to change name of playlist.

--- website/server/publications/publications.js
+++ website/server/publications/publications.js
@@ -3,8 +3,12 @@ Meteor.publish('allPlaylists', function() {
 });
 
 Meteor.publish('playlistSongs', function(id) {
-   return Songs.find({})
+    return Songs.find({playlist: {$elemMatch: {$eq : id}}});
 });
+
+Meteor.publish('getPlaylist', function(owner) {
+    return Playlists.find({owner: owner});
+})
 
 //Meteor.publish('playlistSongs', function(playlistId) {
 //    return Songs.find({
