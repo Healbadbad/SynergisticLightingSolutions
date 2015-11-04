514b8b07da5e75ada441d54ef4128dd7243a665d
Andrew Ma <maastergotmail@gmail.com>
2015-11-03 21:24:59 -0500

    comment 1 line

--- website/server/mp3Player.js
+++ website/server/mp3Player.js
@@ -7,7 +7,7 @@ Meteor.methods({
 
    'playSong': function(file) {
      console.log("play");
-     var Player = Npm.require('player');
+    //  var Player = Npm.require('player');
       //  Meteor.npmRequire('Audio');
 
       //  var audio = new Audio(file);
