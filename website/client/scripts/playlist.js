c8a186e43da0617a58fc69e6b68e86b420e2d6f1
chinchalupa <wrightjt@rose-hulman.edu>
2015-11-04 01:44:30 -0500

    In progress.

--- website/client/scripts/playlist.js
+++ website/client/scripts/playlist.js
@@ -1,5 +1,30 @@
 Template.playlist.helpers({
-  'getSongs' : function(){
-    return this.songs.find();
+  'getSongs' : function() {
+    return Songs.find();
+  }
+});
+
+Template.playlist.events({
+  'remove-song': function(e, template) {
+    files = $("input[type='file']")[0].files[0];
+    var filename = files.name;
+
+    var reader = new FileReader();
+
+    reader.onloadend = function () {
+      var value = reader.result;
+
+      Meteor.call('localUploadAndAnalyze', filename, value);
+
+      Meteor.call('echoUpload', filename, Router.current().location.get().rootUrl, function(err, md5) {
+        Meteor.call('getEchoUrl', md5, function(err, url) {
+          $.getJSON(url, function(data) {
+            console.log(data);
+          });
+        });
+      });
+    };
+    var audio = new Audio(files);
+    reader.readAsBinaryString(files);
   }
 });
--- website/server/publications/publications.js
+++ website/server/publications/publications.js
@@ -2,5 +2,9 @@ Meteor.publish('allPlaylists', function() {
     return Playlists.find({});
 });
 
+Meteor.publish('playlistSongs', function(id) {
+   return Songs.find({})
+});
+
 //Meteor.publish('playlistSongs', function(playlistId) {
 //    return Songs.find({
