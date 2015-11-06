1f6c08c8d492bc7df3adcc05919157259b9cdcc7
Philip Ross <rosspa@rose-hulman.edu>
2015-11-05 21:30:37 -0500

    settings now give alert confirmation

--- website/client/scripts/modals/settingModal.js
+++ website/client/scripts/modals/settingModal.js
@@ -11,20 +11,17 @@ Template.settingsModal.events({
     'click #update-firmware-button': function (e) {
         // TODO
         e.preventDefault();
+        createAlert("You have successfully updated the firmware.");
         $('#update-firmware-button').prop('disabled', true);
-        $("#update-firmware-alert").css("display", "block");
-        console.log("update the firmware");
+        updateFirmware();
     },
     'click #update-number-leds-button': function (e) {
         // TODO
         e.preventDefault();
-        $('body').trigger({
-            type: 'keyup',
-            which: 27 // Escape key
-        });
         e.stopPropagation();
         numberOfLEDs = $('#led-input').val();
-        console.log("save settings");
+        updateLEDNumber(numberOfLEDs);
+        createAlert("you have successfully updated the number of LEDs to " + numberOfLEDs);
     },
     'click .cancel': function (e) {
         e.preventDefault();
@@ -35,10 +32,29 @@ Template.settingsModal.events({
         e.stopPropagation();
     }
     ,
-    'click #update-firmware-alert-close': function (e) {
-        $("#update-firmware-alert").css("display", "none");
+    'click .close': function (e) {
+        $(e.target).parent().remove();
     }
-})
-;
+});
 
+function createAlert(message) {
+    console.log("create alert");
+    var div = $('<div></div>');
+    console.log(1);
+    var alert = $('<div></div>').addClass("alert-box success radius").text(message);
+    console.log(2);
+    alert.val(message);
+    console.log(3);
+    alert.append($('<a></a>').addClass('close').html('&times'));
+    console.log(4);
+    $('#alert-box').append(alert);
+    //TODO test and check
+}
 
+function updateFirmware() {
+    console.log("update the firmware");
+}
+
+function updateLEDNumber(newNumber) {
+    console.log("update the number of leds to " + newNumber);
+}
