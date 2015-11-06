f5ea9d0dec70c83952a840f8b65c101f4df5eeb3
chinchalupa <wrightjt@rose-hulman.edu>
2015-11-05 23:49:46 -0500

    Changes in globals and publications.

--- website/server/globalGlobals.js
+++ website/server/globalGlobals.js
@@ -2,5 +2,5 @@
  * Created by Jeremy on 11/4/2015.
  */
 // Put your IP Address here and it will be read universally
-IPADRESS = "137.112.224.78";
+IPADRESS = "137.112.151.148";
 SONG_ADDRESS = "http://" + IPADRESS + "/songs/";
